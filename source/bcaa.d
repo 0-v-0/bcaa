9b157917e6c7b9fffb406d8d6cb979cdb1a1159b
0-v-0 <kduice@qq.com>
2024-01-09 16:59:22 +0800

    make calcHash static

--- source/bcaa.d
+++ source/bcaa.d
@@ -13,8 +13,8 @@ Simplified betterC port of druntime/blob/master/src/rt/aaA.d
 
 module bcaa;
 
-version(LDC){
-    version(D_BetterC){
+version (LDC) {
+    version (D_BetterC) {
         pragma(LDC_no_moduleinfo);
     }
 }
@@ -22,7 +22,9 @@ version(LDC){
 //import std.experimental.allocator.common : stateSize;
 
 import core.stdc.string;
-private enum {
+
+private:
+enum {
     // grow threshold
     GROW_NUM = 4,
     GROW_DEN = 5,
@@ -36,7 +38,7 @@ private enum {
 // smaller than half the grow threshold to have a hysteresis
 static assert(GROW_FAC * SHRINK_NUM * GROW_DEN < GROW_NUM * SHRINK_DEN);
 
-private enum {
+enum {
     // initial load factor (for literals), mean of both thresholds
     INIT_NUM = (GROW_DEN * SHRINK_NUM + GROW_NUM * SHRINK_DEN) / 2,
     INIT_DEN = SHRINK_DEN * GROW_DEN,
@@ -48,32 +50,47 @@ private enum {
     HASH_FILLED_MARK = size_t(1) << 8 * size_t.sizeof - 1
 }
 
-private {
-    alias hash_t = size_t;
+enum isSomeString(T) = is(immutable T == immutable C[], C) &&
+    (is(C == char) || is(C == wchar) || is(C == dchar));
 
-    enum isSomeString(T) = is(immutable T == immutable C[], C) && (is(C == char) || is(C == wchar) || is(C == dchar));
+template KeyType(K) {
+    alias Key = K;
 
-    template KeyType(K){
-        alias Key = K;
+@nogc nothrow pure:
+    hash_t getHash(in Key key) @safe => key.hashOf;
 
-        @nogc nothrow pure:
-        hash_t getHash(scope const Key key) @safe {
-            return key.hashOf;
-        }
-
-        bool equals(scope const Key k1, scope const Key k2) {
-            static if(is(K == const(char)*)){
-                return strlen(k1) == strlen(k2) &&
-                    strcmp(k1, k2) == 0;
-            } else static if(isSomeString!K){
-                const len = k1.length;
-                return len == k2.length && strncmp(k1.ptr, k2.ptr, len) == 0;
-            } else {
-                return k1 == k2;
-            }
+    bool equals(in Key k1, in Key k2) {
+        static if (is(K == const(char)*)) {
+            return strlen(k1) == strlen(k2) &&
+                strcmp(k1, k2) == 0;
+        } else static if (isSomeString!K) {
+            const len = k1.length;
+            return len == k2.length && strncmp(k1.ptr, k2.ptr, len) == 0;
+        } else {
+            return k1 == k2;
         }
     }
 }
+
+size_t nextpow2(size_t n) pure nothrow @nogc {
+    import core.bitop : bsr;
+
+    if (!n)
+        return 1;
+
+    const isPowerOf2 = !((n - 1) & n);
+    return 1 << (bsr(n) + !isPowerOf2);
+}
+
+size_t mix(size_t h) @safe pure nothrow @nogc {
+    enum m = 0x5bd1e995;
+    h ^= h >> 13;
+    h *= m;
+    h ^= h >> 15;
+    return h;
+}
+
+public:
 
 /// mallocator code BEGINS
 
@@ -82,28 +99,28 @@ private {
 
 struct Mallocator {
     //import std.experimental.allocator.common : platformAlignment;
-    import core.stdc.stdlib: malloc, realloc, free;
+    import core.stdc.stdlib : malloc, realloc, free;
 
     //enum uint alignment = platformAlignment;
 
 static:
+@nogc nothrow:
 
-    void[] allocate(size_t bytes) @trusted @nogc nothrow {
-        if (!bytes) return null;
+    void[] allocate(size_t bytes) @trusted {
+        if (!bytes)
+            return null;
         auto p = malloc(bytes);
         return p ? p[0 .. bytes] : null;
     }
 
-    void deallocate(void[] b) @system @nogc nothrow {
-        free(b.ptr);
-    }
+    void deallocate(void[] b) @system => free(b.ptr);
 
     alias
-        deallocate = free,
-        dispose = deallocate;
+    deallocate = free,
+    dispose = deallocate;
 
-    bool reallocate(ref void[] b, size_t s) @system @nogc nothrow {
-        if (!s){
+    bool reallocate(ref void[] b, size_t s) @system {
+        if (!s) {
             // fuzzy area in the C standard, see http://goo.gl/ZpWeSE
             // so just deallocate and nullify the pointer
             deallocate(b);
@@ -111,8 +128,9 @@ static:
             return true;
         }
 
-        auto p = cast(ubyte*) realloc(b.ptr, s);
-        if (!p) return false;
+        auto p = cast(ubyte*)realloc(b.ptr, s);
+        if (!p)
+            return false;
         b = p[0 .. s];
         return true;
     }
@@ -120,19 +138,16 @@ static:
     Mallocator instance;
 }
 
-T[] makeArray(T, Allocator)(auto ref Allocator alloc, size_t length){
-    return cast(T[])alloc.allocate(length * T.sizeof);
-}
+T[] makeArray(T, Allocator)(auto ref Allocator alloc, size_t length)
+    => cast(T[])alloc.allocate(length * T.sizeof);
 
-T* make(T, Allocator)(auto ref Allocator alloc){
-    return cast(T*)alloc.allocate(T.sizeof).ptr;
-}
+T* make(T, Allocator)(auto ref Allocator alloc)
+    => cast(T*)alloc.allocate(T.sizeof).ptr;
 
 /// mallocator code ENDS
 
 struct Bcaa(K, V, Allocator = Mallocator) {
-
-    struct Node{
+    struct Node {
         K key;
         V val;
 
@@ -143,17 +158,11 @@ struct Bcaa(K, V, Allocator = Mallocator) {
     private pure nothrow @nogc @safe:
         size_t hash;
         Node* entry;
-        @property bool empty() const {
-            return hash == HASH_EMPTY;
-        }
+        @property bool empty() const => hash == HASH_EMPTY;
 
-        @property bool deleted() const {
-            return hash == HASH_DELETED;
-        }
+        @property bool deleted() const => hash == HASH_DELETED;
 
-        @property bool filled() const {
-            return cast(ptrdiff_t) hash < 0;
-        }
+        @property bool filled() const => cast(ptrdiff_t)hash < 0;
     }
 
 private:
@@ -187,7 +196,7 @@ public:
         if (buckets is null || buckets.length == 0)
             return 0;
         int result = 0;
-        foreach (ref b; buckets[firstUsed .. $]){
+        foreach (ref b; buckets[firstUsed .. $]) {
             if (!b.filled)
                 continue;
             result = dg(AAPair!(K, V)(&b.entry.key, &b.entry.val));
@@ -201,29 +210,23 @@ public:
 @nogc nothrow:
     @property pure @safe {
         size_t length() const
-        in(used >= deleted) {
-            return used - deleted;
-        }
+        in (used >= deleted) => used - deleted;
 
-        size_t dim() const {
-            return buckets.length;
-        }
+        size_t dim() const => buckets.length;
 
-        size_t mask() const {
-            return dim - 1;
-        }
+        size_t mask() const => dim - 1;
     }
 
     inout(Bucket)* findSlotInsert(size_t hash) inout pure {
-        for (size_t i = hash & mask, j = 1;; ++j){
+        for (size_t i = hash & mask, j = 1;; ++j) {
             if (!buckets[i].filled)
                 return &buckets[i];
             i = (i + j) & mask;
         }
     }
 
-    inout(Bucket)* findSlotLookup(size_t hash, scope const K key) inout {
-        for (size_t i = hash & mask, j = 1;; ++j){
+    inout(Bucket)* findSlotLookup(size_t hash, in K key) inout {
+        for (size_t i = hash & mask, j = 1;; ++j) {
 
             if (buckets[i].hash == hash && TKey.equals(key, buckets[i].entry.key))
                 return &buckets[i];
@@ -234,12 +237,12 @@ public:
         }
     }
 
-    void set(scope const K key, scope const V val) {
+    void set(in K key, in V val) {
         initTableIfNeeded();
 
         const keyHash = calcHash(key);
 
-        if (auto p = findSlotLookup(keyHash, key)){
+        if (auto p = findSlotLookup(keyHash, key)) {
             p.entry.val = cast(V)val;
             return;
         }
@@ -250,7 +253,7 @@ public:
             --deleted;
 
         // check load factor and possibly grow
-        else if (++used * GROW_DEN > dim * GROW_NUM){
+        else if (++used * GROW_DEN > dim * GROW_NUM) {
             grow();
             p = findSlotInsert(keyHash);
             //assert(p.empty);
@@ -264,19 +267,19 @@ public:
 
         p.hash = keyHash;
 
-        if (p.deleted){
-            p.entry.key = key;
+        if (p.deleted) {
+            p.entry.key = cast(K)key;
             p.entry.val = cast(V)val;
         } else {
             Node* newNode = allocator.make!Node();
-            newNode.key = key;
+            newNode.key = cast(K)key;
             newNode.val = cast(V)val;
 
             p.entry = newNode;
         }
     }
 
-    private size_t calcHash(scope const K pkey) pure {
+    private static size_t calcHash(in K pkey) pure {
         // highest bit is set to distinguish empty/deleted from filled buckets
         const hash = TKey.getHash(pkey);
         return mix(hash) | HASH_FILLED_MARK;
@@ -286,15 +289,13 @@ public:
         auto obuckets = buckets;
         allocHtable(sz);
 
-        foreach (ref b; obuckets[firstUsed .. $]){
+        foreach (ref b; obuckets[firstUsed .. $]) {
             if (b.filled)
                 *findSlotInsert(b.hash) = b;
-            if (b.empty || b.deleted){
+            if (b.empty || b.deleted) {
                 allocator.dispose(b.entry);
-
                 b.entry = null;
             }
-
         }
 
         firstUsed = 0;
@@ -318,12 +319,12 @@ public:
             resize(dim / GROW_FAC);
     }
 
-    bool remove(scope const K key) {
+    bool remove(in K key) {
         if (!length)
             return false;
 
         const hash = calcHash(key);
-        if (auto p = findSlotLookup(hash, key)){
+        if (auto p = findSlotLookup(hash, key)) {
             // clear entry
             p.hash = HASH_DELETED;
             // just mark it to be disposed
@@ -337,29 +338,26 @@ public:
         return false;
     }
 
-    V get(scope const K key) {
-        if(auto ret = key in this)
+    V get(in K key) {
+        if (auto ret = key in this)
             return *ret;
         return V.init;
     }
 
     alias opIndex = get;
 
-    void opIndexAssign(scope const V value, scope const K key) {
+    void opIndexAssign(in V value, in K key) {
         set(key, value);
     }
 
-    static if(isSomeString!K) @property {
-        auto opDispatch(K key)() {
-            return opIndex(key);
+    static if (isSomeString!K)
+        @property {
+            auto opDispatch(K key)() => opIndex(key);
+
+            auto opDispatch(K key)(in V value) => opIndexAssign(value, key);
         }
 
-        auto opDispatch(K key)(scope const V value) {
-            return opIndexAssign(value, key);
-        }
-    }
-
-    V* opBinaryRight(string op : "in")(scope const K key) {
+    V* opBinaryRight(string op : "in")(in K key) {
         if (!length)
             return null;
 
@@ -374,8 +372,8 @@ public:
     K[] keys() {
         K[] ks = allocator.makeArray!K(length);
         size_t j;
-        foreach (ref b; buckets[firstUsed .. $]){
-            if (b.filled){
+        foreach (ref b; buckets[firstUsed .. $]) {
+            if (b.filled) {
                 ks[j++] = b.entry.key;
             }
         }
@@ -388,8 +386,8 @@ public:
     V[] values() {
         V[] vals = allocator.makeArray!V(length);
         size_t j;
-        foreach (ref b; buckets[firstUsed .. $]){
-            if (b.filled){
+        foreach (ref b; buckets[firstUsed .. $]) {
+            if (b.filled) {
                 vals[j++] = b.entry.val;
             }
         }
@@ -397,25 +395,25 @@ public:
         return vals;
     }
 
-    void clear() { // WIP
+    void clear() @trusted { // WIP
         /+ not sure if this works with this port
         import core.stdc.string : memset;
         // clear all data, but don't change bucket array length
         memset(&buckets[firstUsed], 0, (buckets.length - firstUsed) * Bucket.sizeof);
         +/
         // just loop over entire slice
-        foreach(ref b; buckets)
-            if(b.entry){
+        foreach (ref b; buckets)
+            if (b.entry) {
                 allocator.dispose(b.entry);
             }
         deleted = used = 0;
-        firstUsed = cast(uint) dim;
+        firstUsed = cast(uint)dim;
         buckets[] = Bucket.init;
     }
 
-    void free() {
-        foreach(ref b; buckets)
-            if(b.entry){
+    void free() @trusted {
+        foreach (ref b; buckets)
+            if (b.entry) {
                 allocator.dispose(b.entry);
             }
 
@@ -427,9 +425,7 @@ public:
     auto copy() {
         auto newBuckets = allocator.makeArray!Bucket(buckets.length);
         memcpy(newBuckets.ptr, buckets.ptr, buckets.length * Bucket.sizeof);
-        typeof(this) newAA = {
-            newBuckets, firstUsed, used, deleted
-        };
+        typeof(this) newAA = {newBuckets, firstUsed, used, deleted};
         return newAA;
     }
 
@@ -438,7 +434,7 @@ public:
         if (!buckets.length)
             return 0;
         int result = 0;
-        foreach (ref b; buckets[firstUsed .. $]){
+        foreach (ref b; buckets[firstUsed .. $]) {
             if (!b.filled)
                 continue;
             result = dg(AAPair!(K, V)(&b.entry.key, &b.entry.val));
@@ -449,30 +445,27 @@ public:
         return 0;
     }
 
-    struct BCAARange(alias rangeType) {
+    struct BCAARange(string ret) {
         typeof(buckets) bucks;
         size_t len;
         size_t current;
 
-        nothrow @nogc:
+    nothrow @nogc:
 
-        bool empty() const pure @safe {
-            return len == 0;
-        }
+        bool empty() const pure @safe => len == 0;
 
         // front must be called first before popFront
-        auto front(){
-            while(bucks[current].hash <= 0)
+        auto front() {
+            while (bucks[current].hash <= 0)
                 ++current;
 
             auto entry = bucks[current].entry;
-            mixin rangeType!entry;
-            return rangeType;
+            return mixin(ret);
         }
 
-        void popFront(){
-            foreach (ref b; bucks[current .. $]){
-                if (!b.empty){
+        void popFront() {
+            foreach (ref b; bucks[current .. $]) {
+                if (!b.empty) {
                     --len;
                     ++current;
                     break;
@@ -482,20 +475,11 @@ public:
     }
 
     // The following functions return an InputRange
-    auto byKeyValue() {
-        auto rangeType(alias T) = T;
-        return BCAARange!rangeType(buckets, length, firstUsed);
-    }
+    auto byKeyValue() => BCAARange!"entry"(buckets, length, firstUsed);
 
-    auto byKey() {
-        auto rangeType(alias T) = T.key;
-        return BCAARange!rangeType(buckets, length, firstUsed);
-    }
+    auto byKey() => BCAARange!"entry.key"(buckets, length, firstUsed);
 
-    auto byValue() {
-        auto rangeType(alias T) = T.val;
-        return BCAARange!rangeType(buckets, length, firstUsed);
-    }
+    auto byValue() => BCAARange!"entry.val"(buckets, length, firstUsed);
 }
 
 struct AAPair(K, V) {
@@ -503,28 +487,10 @@ struct AAPair(K, V) {
     V* valp;
 }
 
-private size_t nextpow2(size_t n) pure nothrow @nogc {
-    import core.bitop : bsr;
-
-    if (!n)
-        return 1;
-
-    const isPowerOf2 = !((n - 1) & n);
-    return 1 << (bsr(n) + !isPowerOf2);
-}
-
-private size_t mix(size_t h) @safe pure nothrow @nogc {
-    enum m = 0x5bd1e995;
-    h ^= h >> 13;
-    h *= m;
-    h ^= h >> 15;
-    return h;
-}
-
-unittest
-{
+unittest {
     Bcaa!(string, string) aa;
-    scope(exit) aa.free;
+    scope (exit)
+        aa.free;
     aa["foo".idup] = "bar";
     assert(aa.foo == "bar");
 }
@@ -537,23 +503,26 @@ unittest {
     clock_t begin = clock();
     {
         Bcaa!(int, int) aa0;
-        scope(exit) aa0.free;
+        scope (exit)
+            aa0.free;
 
-        foreach (i; 0..1000_000){
+        foreach (i; 0 .. 1000_000) {
             aa0[i] = i;
         }
 
-        foreach (i; 2000..1000_000){
+        foreach (i; 2000 .. 1000_000) {
             aa0.remove(i);
         }
 
         printf("%d\n", aa0[1000]);
     }
-    clock_t end = clock(); printf("Elapsed time: %f \n", double(end - begin) / CLOCKS_PER_SEC);
+    clock_t end = clock();
+    printf("Elapsed time: %f \n", double(end - begin) / CLOCKS_PER_SEC);
 
     {
         Bcaa!(string, string) aa1;
-        scope(exit) aa1.free;
+        scope (exit)
+            aa1.free;
 
         aa1["Stevie"] = "Ray Vaughan";
         aa1["Asım Can"] = "Gündüz";
@@ -561,37 +530,37 @@ unittest {
         aa1["İlter"] = "Kurcala";
         aa1.Ferhat = "Kurtulmuş";
 
-        foreach(pair; aa1){
+        foreach (pair; aa1) {
             printf("%s -> %s", (*pair.keyp).ptr, (*pair.valp).ptr);
         }
 
         if (auto valptr = "Dan" in aa1)
-            printf("%s exists!!!!\n", (*valptr).ptr );
+            printf("%s exists!!!!\n", (*valptr).ptr);
         else
             printf("does not exist!!!!\n");
 
-        assert(aa1.remove("Ferhat") == true);
+        assert(aa1.remove("Ferhat"));
         assert(aa1.Ferhat == null);
-        assert(aa1.remove("Foe") == false);
-        assert(aa1["İlter"] =="Kurcala");
+        assert(!aa1.remove("Foe"));
+        assert(aa1["İlter"] == "Kurcala");
 
         aa1.rehash();
 
-        printf("%s\n",aa1["Stevie"].ptr);
-        printf("%s\n",aa1["Asım Can"].ptr);
-        printf("%s\n",aa1.Dan.ptr);
-        printf("%s\n",aa1["Ferhat"].ptr);
+        printf("%s\n", aa1["Stevie"].ptr);
+        printf("%s\n", aa1["Asım Can"].ptr);
+        printf("%s\n", aa1.Dan.ptr);
+        printf("%s\n", aa1["Ferhat"].ptr);
 
         auto keys = aa1.keys;
-        scope(exit) aa1.allocator.dispose(keys);
-        foreach(key; keys)
+        scope (exit)
+            aa1.allocator.dispose(keys);
+        foreach (key; keys)
             printf("%s -> %s\n", key.ptr, aa1[key].ptr);
 
         // byKey, byValue, and byKeyValue do not allocate
         // They use the range magic of D
-        foreach (pp; aa1.byKeyValue()){
+        foreach (pp; aa1.byKeyValue()) {
             printf("%s: %s\n", pp.key.ptr, pp.value.ptr);
-
         }
 
         struct Guitar {
@@ -599,7 +568,8 @@ unittest {
         }
 
         Bcaa!(int, Guitar) guitars;
-        scope(exit) guitars.free;
+        scope (exit)
+            guitars.free;
 
         guitars[0] = Guitar("Fender");
         guitars[3] = Guitar("Gibson");
@@ -609,14 +579,15 @@ unittest {
 
         printf("%s\n", guitars[356].brand.ptr);
 
-        if(auto valPtr = 3 in guitars)
+        if (auto valPtr = 3 in guitars)
             printf("%s\n", (*valPtr).brand.ptr);
     }
 }
 
 unittest {
     Bcaa!(string, int) aa;
-    scope(exit) aa.free;
+    scope (exit)
+        aa.free;
     aa.foo = 1;
     aa.bar = 0;
     assert("foo" in aa);
@@ -644,17 +615,19 @@ unittest {
     }
 
     Bcaa!(int, S) aas;
-    scope(exit) aas.free;
+    scope (exit)
+        aas.free;
 
-    for(int i = 1024; i < 2048; i++) {
-        aas[i] = S(i, i*2, "caca\0");
+    for (int i = 1024; i < 2048; i++) {
+        aas[i] = S(i, i * 2, "caca\0");
     }
     aas[100] = S(10, 20, "caca\0");
 
     import core.stdc.stdio;
+
     printf(".x=%d .y%d %s\n", aas[100].x, aas[100].y, aas[100].txt.ptr);
 
-    for(int i = 1024; i < 2048; i++) {
+    for (int i = 1024; i < 2048; i++) {
         aas.remove(i);
     }
 }
